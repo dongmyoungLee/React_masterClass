b0b7a1b064b1174fd803bb87d62401f02f749316
dongmyoungLee <pajang1515@daum.net>
2022-02-18 01:20:59 +0900

    220218

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -4,7 +4,7 @@ import Circle from "./Circle";
 function App() {
   return (
     <div>
-      <Circle bgColor="teal"></Circle>
+      <Circle text="banana" bgColor="teal" borderColor="red"></Circle>
       <Circle bgColor="tomato"></Circle>
     </div>
   )
