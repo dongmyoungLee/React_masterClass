80829d3727f33de317aa846f65542cd37ff3844d
dongmyoungLee <pajang1515@daum.net>
2022-02-18 23:45:48 +0900

    study

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,11 +1,33 @@
-import Circle from "./Circle";
-
+import React from 'react'
+import styled from 'styled-components'
 
 function App() {
+
+  const Container = styled.div`
+    background-color: ${(props) => props.theme.bgColor};
+  `
+  const H1 = styled.h1`
+    color : ${(props) => props.theme.textColor};
+  `
+  interface DummyProps {
+    text : string
+    active? : boolean
+  }
+
+  function Dummy({ text, active = false }: DummyProps) {
+    return <h1>{text}</h1>
+  }
+  const onClick = (e:React.MouseEvent<HTMLButtonElement>) => {
+    console.log(e.currentTarget)
+  }
+
   return (
     <div>
-      <Circle text="banana" bgColor="teal" borderColor="red"></Circle>
-      <Circle bgColor="tomato"></Circle>
+      <Container>
+        <H1>protected</H1>
+        <Dummy active text="hello"></Dummy>
+        <button onClick={onClick}>click</button>
+      </Container>
     </div>
   )
 }
